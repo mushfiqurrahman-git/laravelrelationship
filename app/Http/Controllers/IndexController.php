dbbb27b0326b980409fb3256dc51fc408e8b2f46
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-05-26 12:53:11 +0600

    vn

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,10 +9,16 @@ use App\Models\Customer;
 class IndexController extends Controller
 {
     public function index($id){
-        $customer = Customer::find($id);
-        var_dump($customer->name);
-        var_dump($customer->email);
-        var_dump($customer->mobile->model);
+        // $customer = Customer::find($id);
+        // var_dump($customer->name);
+        // var_dump($customer->email);
+        // var_dump($customer->mobile->model);
+
+        // $customer  = Mobile::find($id)->customer;
+        // dd($customer);
+
+        $mobile = Customer::find($id)->mobile;
+        dd($mobile);
 
     }
 }
