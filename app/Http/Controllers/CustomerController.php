f1b7aa39ef54e49b911dd9a50a71a2e6d70d7ffc
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-05-26 12:03:49 +0600

    hjd

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mobile;
+use App\Models\Customer;
+
 
 class CustomerController extends Controller
 {
-    //
+    public function add_customer(){
+        $mobile = new Mobile();
+        $mobile->model = 'BLURDSV';
+
+        $customer = new Customer();
+        $customer->name = 'Fosv';
+        $customer->email = 'tosmdeg@ggv.com';
+
+        $customer->save();
+
+        $customer->mobile()->Save($mobile);
+    }
+
+    public function show_mobile($id){
+        $mobile = Customer::find(1)->mobile;
+        return $mobile;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\MobileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('add-customer', [CustomerController::class,'add_customer']);
+Route::get('show-mobile/{id}',[CustomerController::class,'show_mobile']);
